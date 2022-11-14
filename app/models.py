8a634b6fc91dea862934f833bf2967e66d3daa1e
talino <taz.dhillon@gmail.com>
2022-11-14 00:41:15 -0800

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Customer(models.Model):
+
+    created             = models.DateTimeField(auto_now_add=True)
+    active              = models.BooleanField(default=True)
+    name = models.CharField(max_length=50)
+    acquired_date       = models.DateField()
+    retired_date        = models.DateField(blank=True, null=True)
+    slug                = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Customer, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Customer'
+
+    def __str__(self):
+        return self.name
+
+
+class CustomerLocation(models.Model):
+    created             = models.DateTimeField(auto_now_add=True)
+    active              = models.BooleanField(default=True)
+    customer            = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    address             = models.CharField(max_length=50)
+    site_abbreviation   = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = 'Customer Location'
+
+    def __str__(self):
+        return self.address
+
+
+class JobStatus(models.Model):
+    status              = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.status
+
+
+class Job(models.Model):
+    created             = models.DateTimeField(auto_now_add=True)
+    active              = models.BooleanField(default=True)
+    customer            = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer_location   = models.ForeignKey(CustomerLocation, on_delete=models.CASCADE)
+    request_from        = models.CharField(max_length=20)
+    PO_number           = models.CharField(max_length=20)
+    SC_work_order       = models.CharField(max_length=20)
+    notes               = models.TextField
---
+++ proj/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+
+admin.site.site_header = 'ProjectTracker Admin'
+admin.site.site_title = 'ProjectTracker admin'
+admin.site.index_title = 'ProjectTracker Administration'
+admin.site.site_title = 'ProjectTracker Administration'
+admin.site.site_header = 'ProjectTracker Admin Login'
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app.urls')),
+]
